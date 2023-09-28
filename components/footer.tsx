08deb9e5e5030fc20b416e9176181cf668ecbcf9
Zay <Izpopsonic1@gmail.com>
2023-09-28 10:26:02 -0400

    Updated copyright name

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return <footer className="bg-white border-1">
         <div className="mx-auto-py-10">
             <p className="text-center text-xs text-black">
-                &copy; 2023 Serum, All Rights Reserved.
+                &copy; 2023 Lesiras, All Rights Reserved.
 
             </p>
         </div>
